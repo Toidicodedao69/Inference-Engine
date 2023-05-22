8c8bbaa5b38e03061aa11fd70cae3598ca1b73ef
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2023-05-22 20:41:57 +0700

    Fix program input

--- Program.cs
+++ Program.cs
@@ -7,23 +7,38 @@ namespace InferenceEngine
         public static void Main(string[] args)
         {
             KnowledgeBase kB = new KnowledgeBase();
-            //kB.ReadFile(args[0]);
-            kB.ReadFile("test_HornKB.txt");
+            Algorithm? algorithm = null;
+
+            // Read file
+            kB.ReadFile(args[1]);
+            //kB.ReadFile("test_HornKB.txt");
             //kB.ReadFile("test_genericKB_1.txt");
             //kB.ReadFile("test_genericKB.txt");
             //kB.ReadFile("small_test.txt");
 
-            Algorithm FC = new ForwardChaining(kB);
-            FC.Entails();
-            FC.PrintResult();
+            // Read method
+            switch (args[0].ToLower())
+            {
+                case "fc":
+                    algorithm = new ForwardChaining(kB);
+                    break;
+                case "bc":
+                    algorithm = new BackwardChaining(kB);
+                    break;
+                case "tt":
+                    algorithm = new TruthTable(kB);
+                    break;
+                default:
+                    Console.WriteLine("Method not recognized");
+                    break;
+            }
+            
+            if (algorithm != null)
+            {
+                algorithm.Entails();
+                algorithm.PrintResult();
+            }
 
-            //Algorithm BC = new BackwardChaining(kB);
-            //BC.Entails();
-            //BC.PrintResult();
-
-            //Algorithm TT = new TruthTable(kB);
-            //TT.Entails();
-            //TT.PrintResult();
         }
 
     }
